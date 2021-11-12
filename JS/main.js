ca9d6ff671ec7cccc85fab263deeb3fcf26b3c14
happiness <makenahappiness97@gmail.com>
2021-11-12 19:47:25 +0300

    added variables

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+function fn() {
+    var d = new Date();
+var date = parseInt(document.getElementsByClass("day").value);
+var month = parseInt(document.getElementsByClassName("month").value);
+var year = parseInt(document.getElementsByClassName("year").value);
+
+}
