1806f8f0f2ea1fcaa65c7cc3a07f3ac7c02fe728
happiness <makenahappiness97@gmail.com>
2021-11-13 22:17:45 +0300

    added padding

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,81 @@
 function fn() {
     var d = new Date();
 var date = parseInt(document.getElementsByClass("day").value);
-var month = parseInt(document.getElementsByClassName("month").value);
-var year = parseInt(document.getElementsByClassName("year").value);
+var month = parseInt(document.getElementsByClass("month").value) - 1;
+var year = parseInt(document.getElementsByClass("year").value);
 
-var day = d.getDay();
+if (day < 0 || day > 31) {
+    document.getElementsByClassName("dayerror").innerHtml = "Enter valid date";
+} else {
+    d.setDay(day);
+}
+
+if (month < 0 || month > 12) {
+    document.getElementsByClassName("montherror").innerHtml = "Enter valid month";
+} else {
+    d.setMonth(month);
+}
+
+if (year < 0) {
+    document.getElementsByClassName("yearerror").innerHtml = "Enter valid year"
+}  else {
+    d.setFullYear(year);
+}
+
+var day = d.setDay();
 var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
-var isFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var isMale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 
-function getAkan(){
-    var isMale = document.getElementsByClass("male").checked
-    var isFemale = document.getElementsByClass("female").checked
-    if(isMale == true && isFemale == false){
-        if(day == o){
-            alert("Akan name is" = male(0));
+function getAkan() {
+    var ismale = document.getElementsByClass("male").checked;
+    var isfemale = document.getElementsByClass("female").checked;
+    if (ismale == true && isfemale == false) {
+        if (day == 0) {
+            alert("Akan name is " .concat() male[0]);
         }
-        if(day == 1){
-            alert("Akan name is" = male(1));
+        else if (day == 1) {
+            alert("Akan name is " .concat() male[1]);
         }
-        if(day == 2){
-            alert("Akan name is" = male(2));
+        else if (day == 2) {
+            alert("Akan name is " .concat() male[2]);
         }
-        if(day == 3){
-            alert("Akan name is" == male(3));
+        else if (day == 3) {
+            alert("Akan name is " .concat() male[3])
+        }        
+        else if (day == 4) {
+            alert("Akan name is " .concat() male[4]);
         }
-        if(day == 4){
-            alert("Akan name is" == male(4));
+        else if (day == 5) {
+            alert("Akan name is " .concat() male[5]);
         }
-        if(day == 5){
-            alert("Akan name is" == male(5));
-        }
-        if(day == 6){
-            alert("Akan name is" == male(6));     
+        else if (day == 6) {
+            alert("Akan name is " .concat() Male[6]);     
         }
     }
-    
-}
+   else if (ismale == false && isfemale == true) {
+       if (day == 0) {
+           alert("Akan name is " .concat() female[0]);
+       }
+       else if (day == 1) {
+           alert("Akan name is " .concat() female[1]);
+       }
+       else if (day == 2) {
+           alert("Akan name is " .concat() female[2]);
+       }
+       else if (day == 3) {
+           alert("Akan name is " .concat() female[3]);
+       }
+       else if (day == 4) {
+           alert("Akan name is " .concat() female[4]);
+       } 
+       else if (day == 5) {
+           alert("Akan name is " .concat() female[5]);
+       }
+       else if (day == 6) {
+           alert("Akan name is " .concat() female[6]);
+       }
+    }
+ }
+ getAkan();
 }
